9dbb210526722d575cd3aede73c610331d4e5ccb
marius leclercq <zigame.leclercq@gmail.com>
2024-02-22 16:02:11 +0100

    exo2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to list_url(@list), notice: "List was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
